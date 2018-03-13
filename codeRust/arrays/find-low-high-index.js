6f8609109019a2fdb90ac017d515e3be721d24a3
rcornacchia <rlc2160@columbia.edu>
2018-03-12 21:38:45 -0400

    added coderust problems

---
+++ codeRust/arrays/find-low-high-index.js
@@ -0,0 +1,18 @@
+/**
+ * Given a sorted array of integers, return the low and high index of the given key.
+ * Return -1 if not found. The array length can be in millions with lots of duplicates.
+ * Ex) given key 5 and array = [1, 2, 5, 5, 5, 8] => return [2, 4]
+ */
+
+function findRange(arr, key) {
+  if (!arr || !Array.isArray(arr)) return -1
+  let index = binarySearch(arr, key)
+  if (index < 0) return -1
+  let low = high = index
+  for (let i = index-1; i >= 0 && arr[i] == key; i--) {
+    low = i
+  }
+  for (let i = index+1; i < arr.length && arr[i] == key; i++) {
+    high = i
+  }
+}
--- codeRust/arrays/searchRotated.js
+++ codeRust/arrays/searchRotated.js
@@ -1,5 +1,16 @@
-const unsortedArr = [123, 155, 177, 195, 200, 1, 2, 3, 5, 8, 9, 19]
+/**
+ * General approach is just like binary search.
+ * Check the middle of the array.
+ * One half of the array will be sorted and the other half will be unsorted.
+ * If it's in the sorted half of the array go that way and you have normal binary search
+ * Otherwise go to the other half of the array and check again
+ */
+
 
 const searchRotated = arr => {
   
 }
+
+const rotatedArr = [123, 155, 177, 195, 200, 1, 2, 3, 5, 8, 9, 19]
+const result = searchRotated(rotatedArr)
+console.log(result)

0aaf953ed5b4ec7a2adb0aa468620adc7f1f1fc3
rcornacchia <rlc2160@columbia.edu>
2018-04-20 17:08:45 -0400

    added another array problem

---
+++ codeRust/arrays/moveZerosLeft.js
@@ -0,0 +1,20 @@
+const moveZerosLeft = arr => {
+  let read = arr.length-1
+  let write = arr.length-1
+  while (read >= 0) {
+    if (arr[read] != 0 && read != write) {
+      swap(arr, read, write)
+      write--
+    }
+    read--
+  }
+  return arr
+}
+
+const swap = (arr, pos1, pos2) => {
+  let temp = arr[pos2]
+  arr[pos2] = arr[pos1]
+  arr[pos1] = temp
+}
+
+console.log(moveZerosLeft([10, 7, 0, 3, 0]))
